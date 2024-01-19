70ed3ab3b344695d1ca2fa1f868b929cba90da1b
a89289289 <a89289289@gmail.com>
2024-01-19 17:38:22 +0800

    java

--- BoardGameLogDemo/src/main/java/com/example/psi/repository/GameRecordRepository.java
+++ BoardGameLogDemo/src/main/java/com/example/psi/repository/GameRecordRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.psi.entity.GameRecord;
 
+
 public interface GameRecordRepository extends JpaRepository<GameRecord, Long> {
-    // 可以新增特定的查詢方法，如果有需要
+
 }
--- BoardGameLogDemo/src/main/java/com/example/psi/repository/PlayerJpaRepository.java
+++ BoardGameLogDemo/src/main/java/com/example/psi/repository/PlayerJpaRepository.java
@@ -2,9 +2,11 @@ package com.example.psi.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.psi.entity.Player;
 
+@Repository
 public interface PlayerJpaRepository extends JpaRepository<Player, Long> {
     // 可以新增特定的查詢方法，如果有需要
 }
